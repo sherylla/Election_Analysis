ca3dafa7c9eaec297f394d1e5a6d2b543ee0f486
Sheryll Agpalo <sagpalo@salesforce.com>
2020-02-02 21:34:48 -0800

    syncing all files

---
+++ PyPoll.py
@@ -0,0 +1,90 @@
+import csv
+import os
+
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+#  Initialize.
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+
+     # To do: read and analyze data here.
+     # Read the file object with the reader function.
+     file_reader = csv.reader(election_data)
+
+     # Read and print the header row.
+     headers = next(file_reader)
+
+     # Print each row in the CSV file.
+     for row in file_reader:
+         # 2. Add to the total vote count.
+         total_votes += 1 
+
+         # Print the candidate name from each row.
+         candidate_name = row[2]
+
+         # If the candidate does not match any existing candidate...
+         if candidate_name not in candidate_options:
+             # Add it to the list of candidates.
+             candidate_options.append(candidate_name)
+         # 2. Begin tracking that candidate's vote count. 
+             candidate_votes[candidate_name] = 0
+
+         # Add a vote to that candidate's count.
+         candidate_votes[candidate_name] += 1
+        
+         # Save the results to our text file.
+with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+
+
+# 1. Iterate through the candidate list.
+    for candidate in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate]
+    # 3. Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+        candidate_results = (
+            f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        #  Save the candidate results to our text file.
+        txt_file.write(candidate_results)
+
+   # print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+         # And, set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate
+
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
